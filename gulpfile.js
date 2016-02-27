c06b9534b65f787cd8c1d880e7936166c2e7b7bb
Mr. Leo <leo@users.noreply.github.com>
2016-02-27 11:48:42 +0100

    Asset pipeline added

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
+const concat = require('gulp-concat')
+
+const browserSync = require('browser-sync')
+
+const paths = {
+  scss: 'assets/scss/**/*.scss',
+  js: 'assets/js/**/*.js',
+  images: 'assets/images/**/*'
+}
+
+gulp.task('styles', () => {
+  return gulp
+    .src(paths.scss)
+    .pipe(concat('styles.scss'))
+    .pipe(sass({
+      outputStyle: 'compressed',
+      includePaths: ['assets/scss']
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('js', () => {
+  return gulp
+    .src(paths.js)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('scripts', ['js'], browserSync.reload)
+
+gulp.task('images', () => {
+  return gulp
+    .src(paths.images)
+    .pipe(gulp.dest('dist/images'))
+})
+
+gulp.task('watch', () => {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  })
+
+  gulp.watch(paths.scss, ['styles'])
+  gulp.watch(paths.js, ['scripts'])
+  gulp.watch(paths.images, ['images'])
+})
+
+gulp.task('default', ['styles', 'js', 'images'])
