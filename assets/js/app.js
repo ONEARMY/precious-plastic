ec9d5ea58a72a586f6b62a3afd6576ff38a35192
Leo <leo@users.noreply.github.com>
2016-03-18 12:42:34 +0100

    Loads news
    This closes #2

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,45 @@ $(document).ready(function() {
     $(this).toggleClass('on')
   })
 
+  if ($('#news')) {
+    $.ajax({
+      type: 'GET',
+      url: 'http://davehakkens.nl/tag/preciousplastic/feed/',
+      success: function (xml) {
+        $(xml).find('item').each(function (index) {
+          var article = $('<article />')
+
+          var title = $(this).find('title').text()
+          var description = $($(this).find('description').text())[1]
+          var url = $(this).find('link').text()
+
+          var preview = $($(this).find('description').text()).find('.wp-post-image')
+          var figure = $('<figure></figure>')
+
+          figure.append(preview)
+          article.append(figure)
+
+          article.append('<h1>' + title + '</h1>')
+
+          var descText = $(description).text()
+
+          if (descText.indexOf('-source') == -1 && descText.indexOf(title) == -1) {
+            article.append('<p>' + descText + '</p>')
+          }
+
+          var news = $('#news .container:first-child')
+
+          news.find('.loading').hide()
+          news.append(article)
+
+          if (index == 2) {
+            return false;
+          }
+        })
+      }
+    })
+  }
+
   if ($().accordion) {
     $('#page dl').accordion()
   }
