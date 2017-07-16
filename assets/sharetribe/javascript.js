cdc89d397a721f358caa80e84825dfd41152cacf
Dave Hakkens <mail@davehakkens.nl>
2017-07-16 14:05:18 +0200

    remove sharetribe
    Moved sharetribe assets to marketplace
    https://hakkens.github.io/precious-plastic-market/Sharetribe/

--- assets/sharetribe/javascript.js
+++
@@ -1,8 +0,0 @@
-$(".col-12 ul").text(function () {
-    return $(this).text().replace("Pickup", "Domestic or pickup ");
-});
-
-
-$(".col-12 ul").text(function () {
-    return $(this).text().replace("Shipping", "International Shipping");
-});
